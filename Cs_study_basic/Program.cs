1546c6e38202ffbd52438ba173f899025c4f6162
yuichikano <58720709+yuichikano@users.noreply.github.com>
2021-05-15 22:43:25 +0900

    a

---
+++ Cs_study_basic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Cs_study_basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lst = new List<string>();
+            string a = null;
+            string output_path = "./out.txt";
+            
+            while(a != "end"){
+                Console.WriteLine("入力:");
+                a = Console.ReadLine();
+                lst.Add(a);
+            }
+
+            foreach(string x in lst){
+                Console.WriteLine(x);
+            }
+
+            if(!File.Exists(output_path)){
+                File.WriteAllLines(output_path, lst);
+            }
+
+        }
+    }
+}
